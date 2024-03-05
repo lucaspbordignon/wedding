72d7aeff1ee5f6bc8792c922cfcd2bf1756fb285
Lucas Bordignon <bordi.lucas@gmail.com>
2024-03-04 23:38:27 -0300

    app | Adjust minimum resolution for video

--- src/app/components/OverlayVideo.tsx
+++ src/app/components/OverlayVideo.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import MuxPlayer from "@mux/mux-player-react";
+import MuxPlayer, { MinResolution } from "@mux/mux-player-react";
 
 const OverlayVideo = () => {
   return (
     <MuxPlayer
       className="w-screen h-screen absolute -z-10 object-cover"
       playbackId="URUZfqcvQ2vN900m9JpPo9iSLZZZc3gGpf5Wvenlqq98"
+      minResolution={MinResolution.noLessThan1080p}
       loop
       muted
       autoPlay
