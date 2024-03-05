7346b2caadd9fc01c3a7cc8e7134512aade76f2a
Lucas Bordignon <bordi.lucas@gmail.com>
2024-03-04 23:19:54 -0300

    app | Add logo to the header

--- src/app/components/SaveTheDateContent.tsx
+++ src/app/components/SaveTheDateContent.tsx
@@ -1,15 +1,21 @@
 const SaveTheDateContent = () => {
   return (
     <section className="flex flex-col w-full h-screen items-center justify-center gap-20">
-      <div className="flex flex-col items-center justify-center text-7xl">
-        <h1>29</h1>
-        <h1>mar</h1>
-        <h1>25</h1>
+      <div className="flex gap-4">
+        <div className="-rotate-90 font-sans text-lg font-light h-min m-auto tracking-[.35rem]">
+          SAVE THE DATE
+        </div>
+
+        <div className="flex flex-col justify-center text-7xl">
+          <h1 className="-mb-3">29</h1>
+          <h1>mar</h1>
+          <h1>25</h1>
+        </div>
       </div>
 
-      <div className="flex flex-col items-center justify-center font-sans gap-1">
+      <div className="flex flex-col justify-center font-sans gap-1">
         <p className="text-2xl font-medium">Gabriela & Lucas</p>
-        <p className="text-xl">Florianópolis - SC</p>
+        <p className="text-xl font-thin">Florianópolis - SC</p>
       </div>
     </section>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import OverlayVideo from "@wedding/app/components/OverlayVideo";
 import SaveTheDateContent from "@wedding/app/components/SaveTheDateContent";
+import Logo from "@wedding/app/components/Logo";
 
 const Home = () => {
   return (
     <main className="flex min-h-screen min-w-screen font-header">
+      <section key="navbar" className="fixed top-0 w-full">
+        <Logo className="m-6" />
+      </section>
+
       <OverlayVideo />
 
       <SaveTheDateContent />
