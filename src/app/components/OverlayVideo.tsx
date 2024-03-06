683c5b1be12d9248aefcb165cf3261cec8ace551
Lucas Bordignon <bordi.lucas@gmail.com>
2024-03-05 21:27:50 -0300

    app | Set opacity transition on first load

--- src/app/components/OverlayVideo.tsx
+++ src/app/components/OverlayVideo.tsx
@@ -1,24 +1,31 @@
 "use client";
 
 import MuxPlayer, { MinResolution } from "@mux/mux-player-react";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
 interface OverlayVideoProps {
   onPlaying: () => void;
 }
 
 const OverlayVideo: FC<OverlayVideoProps> = ({ onPlaying }) => {
+  const [opacity, setOpacity] = useState(1);
+
   setTimeout(() => {
     /* Guarantees the callback is executed on low-battery mode */
     onPlaying();
   }, 1000);
 
+  useEffect(() => {
+    setOpacity(0.6);
+  }, []);
+
   return (
     <MuxPlayer
-      className="w-screen h-screen absolute -z-10 object-cover opacity-70"
+      className="w-screen h-screen absolute -z-10 object-cover transition-opacity duration-4000"
       playbackId="URUZfqcvQ2vN900m9JpPo9iSLZZZc3gGpf5Wvenlqq98"
       poster="https://image.mux.com/URUZfqcvQ2vN900m9JpPo9iSLZZZc3gGpf5Wvenlqq98/thumbnail.webp?time=0"
       minResolution={MinResolution.noLessThan1080p}
+      style={{ opacity }}
       loop
       muted
       autoPlay
