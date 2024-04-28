e21f18a2eb615c43292c5c575ef142c40e61479b
Lucas Bordignon <bordi.lucas@gmail.com>
2024-04-28 12:47:56 -0300

    app | Add initial aniation for the words on the home

--- src/components/BannerEventDate/index.tsx
+++ src/components/BannerEventDate/index.tsx
@@ -1,10 +1,39 @@
+import { animated } from "@react-spring/web";
+import usePageState from "@wedding/state/page";
+
 const BannerEventDate = () => {
+  const scrollYProgress = usePageState((state) => state.scrollYProgress);
+
+  const windowHeight = window.innerHeight;
+  const windowWidth = window.innerWidth;
+
+  const scrollYProgressPercentage = scrollYProgress / windowHeight;
+
+  const biasFactorTranslate = -32;
+  const biasFactorLetterSpacing = Math.min(
+    windowWidth * 0.05,
+    (scrollYProgressPercentage + 0.1) * windowWidth * 0.05
+  );
+
+  const translateX = Math.min(
+    (windowWidth / 2) * 0.75,
+    scrollYProgressPercentage * (windowWidth / 2) - biasFactorTranslate
+  );
+
   return (
     <section className="flex justify-center text-7xl">
-      <div className="ml-16">
-        <h1>29</h1>
-        <h1>mar</h1>
-        <h1>25</h1>
+      <div>
+        <animated.h1
+          style={{ translateX: translateX + biasFactorLetterSpacing }}
+        >
+          29
+        </animated.h1>
+        <animated.h1 style={{ translateX: translateX }}>mar</animated.h1>
+        <animated.h1
+          style={{ translateX: translateX + biasFactorLetterSpacing }}
+        >
+          25
+        </animated.h1>
       </div>
     </section>
   );
