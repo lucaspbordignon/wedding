e455e0161607ad04ab2a79e9fb647f95b906107f
Lucas Bordignon <bordi.lucas@gmail.com>
2024-06-23 17:40:33 -0300

    app | Add @vercel/kv setup

--- src/app/rsvp/page.tsx
+++ src/app/rsvp/page.tsx
@@ -1,7 +1,22 @@
-const Page = () => (
-  <main>
-    <h1>Nothing to see here 👀</h1>
-  </main>
-);
+import { kv } from "@vercel/kv";
+
+const Page = () => {
+  const confirmAttendance = async () => {
+    "use server";
+
+    await kv.set(
+      "attendance",
+      JSON.stringify({ name: "John Doe", confirmed: true })
+    );
+  };
+
+  return (
+    <main>
+      <button onClick={confirmAttendance}>
+        Click me to add a piece of data
+      </button>
+    </main>
+  );
+};
 
 export default Page;
