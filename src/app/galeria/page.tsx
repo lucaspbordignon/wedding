8d6191a26790d7a1fd2ff8b85a98fc6ed6d699bc
Lucas Bordignon <bordi.lucas@gmail.com>
2025-08-16 22:05:57 -0300

    app | Add galery photos

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -8,81 +8,77 @@ const Page = () => (
 
     <section className="grid grid-cols-2">
       <Link
-        href="https://alanvieira.pixieset.com/gabriela-e-lucas/f/37688964/"
+        href="https://alanvieira.pixieset.com/gabriela-e-lucas/cerimonia/"
         target="_blank"
       >
-        <div className="col-span-1 h-[40vh] relative bg-primary">
+        <div className="col-span-1 h-[40vh] md:min-w-72 relative bg-primary">
           <span className="text-white font-header text-xl absolute top-[40%] left-8 md:left-16 z-20">
-            cerimônia religiosa
+            cerimônia na praia
           </span>
 
           <img
-            src="/assets/cover-b.jpg"
+            src="/assets/cerimonia.jpg"
             loading="eager"
             className="object-cover size-full opacity-40"
           />
         </div>
       </Link>
 
-      <div className="col-span-1 h-[40vh] size-full bg-primary px-6 py-8 md:pt-16">
-        <h1 className="text-3xl font-header text-white font-medium">Galeria</h1>
+      <Link
+        href="https://alanvieira.pixieset.com/gabriela-e-lucas/recepcaofesta/"
+        target="_blank"
+      >
+        <div className="col-span-1 h-[40vh] md:min-w-72 relative bg-primary">
+          <span className="text-white font-header text-xl absolute top-[40%] left-8 md:left-16 z-20">
+            a festa <br /> pelo fotógrafo
+          </span>
 
-        <p className="text-white text-sm md:text-base mt-4">
-          Após o casamento vamos compartilhar as fotos da festa por aqui.
-        </p>
-
-        <p className="text-white text-sm md:text-base mt-4">
-          Enquanto isso, os registros da nossa cerimônia religiosa já estão disponíveis.
-        </p>
-
-        <svg
-          className="size-16"
-          width="38"
-          height="11"
-          viewBox="0 0 38 11"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M5.56348 5.90278C15.6059 5.90278 23 8 36.5 5"
-            stroke="white"
-            stroke-width="2"
-            stroke-linecap="round"
+          <img
+            src="/assets/festa.jpg"
+            loading="eager"
+            className="object-cover size-full opacity-40"
           />
-          <path
-            d="M1.57397 6.13354C3.11421 4.6036 3.9081 2.53223 5.44825 1.00239C5.53974 0.911509 6.02222 3.44159 6.02222 3.8847C6.02222 4.75857 6.05081 5.57684 6.15774 6.42652C6.26502 7.27907 6.15774 8.63855 6.15774 9.5C6.15774 10.0468 4.28246 8.22957 4.01333 8.05772C2.99176 7.40539 2.15615 6.94394 1 6.56113"
-            stroke="white"
-            stroke-width="2"
-            stroke-linecap="round"
+        </div>
+
+      </Link>
+
+      <Link
+        href="https://drive.google.com/drive/folders/15Jg7LQjJSEsKsPlJOaU10Vi99ksZ28z5?usp=sharing"
+        target="_blank"
+      >
+        <div className="col-span-1 h-[40vh] md:min-w-72 relative bg-primary">
+          <span className="text-white font-header text-xl absolute top-[40%] left-8 md:left-16 z-20">
+            a festa <br />
+            pelos convidados
+            <p className="font-thin font-sans text-sm">câmeras analógicas</p>
+          </span>
+
+          <img
+            src="/assets/analogicas.jpg"
+            loading="eager"
+            className="object-cover size-full opacity-40"
           />
-        </svg>
-      </div>
+        </div>
+      </Link>
 
-      <div className="col-span-1 h-[40vh] relative bg-primary">
-        <span className="text-white font-header text-xl absolute top-[40%] left-8 md:left-16 z-20">
-          a festa <br />
-          pelos convidados
-          <p className="font-thin font-sans text-sm">câmeras analógicas</p>
-        </span>
 
-        <img
-          src="/assets/galery-placeholder.jpg"
-          loading="eager"
-          className="object-cover size-full opacity-40"
-        />
-      </div>
+      <Link
+        href="https://alanvieira.pixieset.com/gabriela-e-lucas/religioso/"
+        target="_blank"
+      >
+        <div className="col-span-1 h-[40vh] md:min-w-72 relative bg-primary">
+          <span className="text-white font-header text-xl absolute top-[40%] left-8 md:left-16 z-20">
+            cerimônia religiosa
+          </span>
 
-      <div className="col-span-1 h-[40vh] relative bg-primary">
-        <span className="text-white font-header text-xl absolute top-[40%] left-8 md:left-16 z-20">
-          a festa <br /> pelo fotógrafo
-        </span>
+          <img
+            src="/assets/cover-c.jpg"
+            loading="eager"
+            className="object-cover size-full opacity-40"
+          />
+        </div>
+      </Link>
 
-        <img
-          src="/assets/cover-c.jpg"
-          loading="eager"
-          className="object-cover size-full opacity-40"
-        />
-      </div>
     </section>
   </ContainerBase>
 );
